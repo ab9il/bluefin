0d72687283232c618b3a61e3f12b35ac762c4d71
Philip Colllier <redcrown01@gmail.com>
2026-06-07 06:20:28 +0000

    change copr for veracrypt

--- build_files/base/99-packages.sh
+++ build_files/base/99-packages.sh
@@ -46,7 +46,7 @@ echo "Installing ${#FEDORA_PACKAGES[@]} packages from Fedora repos..."
 dnf -y install "${FEDORA_PACKAGES[@]}"
 
 # From robot/veracrypt
-copr_install_isolated "architektapx/veracrypt" "veracrypt"
+copr_install_isolated "bigjapka/VeraCrypt" "veracrypt"
 
 # From lukasfr/ripgrep-all
 copr_install_isolated "lukasfr/ripgrep-all" "ripgrep-all"
